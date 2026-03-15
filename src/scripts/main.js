ea5ef8d90dd21479aa3f4c7b175ebca0ecc8ed8e
Danylo Skepskyi <skepskyidanylo@gmail.com>
2026-03-15 17:07:21 +0100

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,19 @@
 'use strict';
 
-// write code here
+const forms = document.querySelectorAll('form');
+
+for (const form of [...forms]) {
+  for (const div of [...form.children]) {
+    const input = div.firstElementChild;
+    const label = document.createElement('label');
+
+    if (input.nodeName !== 'BUTTON') {
+      label.className = 'field-label';
+      label.textContent = input.getAttribute('name');
+      label.setAttribute('for', input.getAttribute('id'));
+      div.prepend(label);
+
+      input.setAttribute('placeholder', input.getAttribute('name'));
+    }
+  }
+}
